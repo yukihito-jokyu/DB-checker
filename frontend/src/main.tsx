471aba78b84afbe7d1815d4880045b258627a629
jokyu yukihito <ok220020@oks.iput.ac.jp>
2026-08-01 20:59:01 +0900

    🎨 アプリケーションの基本スタイルとエントリーポイントを更新 #48

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "@/app/router";
 import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+createRoot(document.getElementById("app") as HTMLElement).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
